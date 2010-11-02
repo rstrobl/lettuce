a6bd2a74d4a073056d7d11042daea7b6d1aeeb45
Francisco Souza <francisco@franciscosouza.net>
2010-11-02 14:49:30 -0200

    Minor changes on espinafre tests

--- tests/integration/test_espinafre.py
+++ tests/integration/test_espinafre.py
@@ -4,8 +4,8 @@ from lettuce.fs import FileSystem
 
 current_directory = FileSystem.dirname(__file__)
 
-def test_test_database_creation():
-    "creating and destroying test database with the -t param on harvest"
+def test_test_database_creation_and_migration():
+    "creating, migrating (with south) and destroying test database with the -t param on harvest"
     FileSystem.pushd(current_directory, 'django', 'espinafre')
 
     status, out = commands.getstatusoutput("python manage.py harvest --test-database --verbosity=3 --apps=leaves")
@@ -15,4 +15,5 @@ def test_test_database_creation():
     lines = out.splitlines()
     assert_equals(lines[0], u"Django's builtin server is running at 0.0.0.0:8000")
     assert_equals(lines[1], u"Setting up a test database...")
+    assert_equals(lines[3], u"Syncing...")
     assert u"Destroying test database..." in out
